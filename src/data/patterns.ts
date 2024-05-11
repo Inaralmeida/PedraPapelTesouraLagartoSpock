127bb171ac6f700ae79f9ae49e1103f034c91039
inaralmeida <inara.ralmeida@gmail.com>
2024-05-11 15:09:27 -0300

    fix: add types

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -1,5 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const modeGameData: any = {
+interface Option {
+  name: string
+  value: string
+  icon: string
+}
+
+interface Rule {
+  [key: string]: string
+}
+
+interface GameData {
+  level: string
+  name: string[]
+  description: string
+  options: Option[]
+  rules: {
+    [key: string]: Rule
+  }
+}
+
+export const modeGameData: {
+  starter: GameData
+  advanced: GameData
+} = {
   starter: {
     level: 'Iniciante',
     name: ['pedra', 'papel', 'tesoura'],
@@ -107,75 +129,5 @@ export const modeGameData: any = {
         tesoura: 'Vitoria'
       }
     }
-  }
-}
-
-export const descriptionRules: {
-  [key: string]: {
-    description: string
-    op1: string
-    op2: string
-    win: string
-  }
-} = {
-  papelpedra: {
-    description: 'Papel cobre pedra',
-    op1: 'Papel',
-    op2: 'Pedra',
-    win: 'Papel'
-  },
-  tesourapapel: {
-    description: 'Tesoura corta papel',
-    op1: 'Tesoura',
-    op2: 'Papel',
-    win: 'Tesoura'
-  },
-  pedralagarto: {
-    description: 'Pedra esmaga lagarto',
-    op1: 'Pedra',
-    op2: 'Lagarto',
-    win: 'Pedra'
-  },
-  lagartospock: {
-    description: 'Lagarto envenena Spock',
-    op1: 'Lagarto',
-    op2: 'Spock',
-    win: 'Lagarto'
-  },
-  spocktesoura: {
-    description: 'Spock derrete tesoura',
-    op1: 'Spock',
-    op2: 'Tesoura',
-    win: 'Spock'
-  },
-  tesouralagarto: {
-    description: 'Tesoura decapita lagarto',
-    op1: 'Tesoura',
-    op2: 'Lagarto',
-    win: 'Tesoura'
-  },
-  lagartopapel: {
-    description: 'Lagarto come papel',
-    op1: 'Lagarto',
-    op2: 'Papel',
-    win: 'Lagarto'
-  },
-  papelspock: {
-    description: 'Papel refuta Spock',
-    op1: 'Papel',
-    op2: 'Spock',
-    win: 'Papel'
-  },
-  spockpedra: {
-    description: 'Spock vaporiza pedra',
-    op1: 'Spock',
-    op2: 'Pedra',
-    win: 'Spock'
-  },
-  pedratesoura: {
-    description: 'Pedra quebra tesoura',
-    op1: 'Pedra',
-    op2: 'Tesoura',
-    win: 'Pedra'
   }
 }
