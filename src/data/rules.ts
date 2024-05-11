01d66fc452c4f220f536d5162a2b093b7e60b71e
inaralmeida <inara.ralmeida@gmail.com>
2024-05-11 15:08:43 -0300

    feat: add rules

---
+++ src/data/rules.ts
@@ -0,0 +1,12 @@
+export const rulesArray = [
+  { win: 'pedra', message: 'Esmaga', loser: 'tesoura' },
+  { win: 'tesoura', message: 'Corta', loser: 'papel' },
+  { win: 'papel', message: 'Refuta', loser: 'spock' },
+  { win: 'spock', message: 'Vaporiza', loser: 'pedra' },
+  { win: 'pedra', message: 'Esmaga', loser: 'lagarto' },
+  { win: 'lagarto', message: 'Come', loser: 'spock' },
+  { win: 'spock', message: 'Vaporiza', loser: 'tesoura' },
+  { win: 'tesoura', message: 'Corta', loser: 'lagarto' },
+  { win: 'lagarto', message: 'Come', loser: 'papel' },
+  { win: 'papel', message: 'Cobre', loser: 'pedra' }
+]
